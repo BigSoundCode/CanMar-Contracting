6d0b03155ff373aeab459f7055e702a3e11ccdf5
BigSoundCode <95320434+BigSoundCode@users.noreply.github.com>
2023-05-15 18:28:45 -0400

    Add files via upload

--- static/events.js
+++ static/events.js
@@ -24,4 +24,56 @@ $(document).ready(function(){
     });
 });
 
+$('.expand-button').on('click', function(){
+  $('.special-text').toggleClass('-expanded');
   
+  if ($('.special-text').hasClass('-expanded')) {
+    $('.expand-button').html('-');
+  } else {
+    $('.expand-button').html('+');
+  }
+});
+
+$('.expand-button-2').on('click', function(){
+  $('.special-text-2').toggleClass('-expanded');
+  
+  if ($('.special-text-2').hasClass('-expanded')) {
+    $('.expand-button-2').html('-');
+  } else {
+    $('.expand-button-2').html('+');
+  }
+});
+
+
+$('.expand-button-3').on('click', function(){
+  $('.special-text-3').toggleClass('-expanded');
+  
+  if ($('.special-text-3').hasClass('-expanded')) {
+    $('.expand-button-3').html('-');
+  } else {
+    $('.expand-button-3').html('+');
+  }
+});
+
+$('.expand-button-4').on('click', function(){
+  $('.special-text-4').toggleClass('-expanded');
+  
+  if ($('.special-text-4').hasClass('-expanded')) {
+    $('.expand-button-4').html('-');
+  } else {
+    $('.expand-button-4').html('+');
+  }
+});
+
+
+$('.expand-button-5').on('click', function(){
+  $('.special-text-5').toggleClass('-expanded');
+  
+  if ($('.special-text-5').hasClass('-expanded')) {
+    $('.expand-button-5').html('-');
+  } else {
+    $('.expand-button-5').html('+');
+  }
+});
+
+
